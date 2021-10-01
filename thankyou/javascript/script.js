6526c67023cfb6a4017ea26ba4a800a25a247c87
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-10-01 22:27:58 +0800

    Modified HTML, CSS and JS files

--- thankyou/javascript/script.js
+++ thankyou/javascript/script.js
@@ -3,16 +3,41 @@ function getRandomColor() {
 }
 
 function changeEverything() {
-    document.getElementById("GIF-media").src="media/RickAstleyGIF.gif";
-
+    let mainHeaderIcon = document.getElementById("header-icon");
+    let mainHeaderText = document.getElementById("header-text");
     let header = document.getElementById("heading");
     let msg = document.getElementById("message");
+
+    // Chaning things in the header
+    mainHeaderIcon.remove();
+    mainHeaderText.textContent = "Gotcha!";
+
+    // Changing main GIF
+    document.getElementById("GIF-media").src="media/RickAstleyGIF.gif";
+
+    // Adding GIFs decor
+    let leftPic = document.createElement('img');
+    leftPic.src="media/RickHead.gif";
+    document.getElementById('leftpic').appendChild(leftPic);
+
+    let rightPic = document.createElement('img');
+    rightPic.src="media/RickHead.gif";
+    document.getElementById('rightpic').appendChild(rightPic);
+
+    let bottomPic = document.getElementById("bottompic");
+    let topPic = document.getElementById("toppic");
+    topPic.src="media/RickRoll.gif";
+    bottomPic.src="media/RickRoll.gif";
+
+    // == Message ==
     header.textContent = "You got rickrolled";
     msg.textContent = "Thank you for participating!";
     
+    // == Changing BG color ==
     let gradiantbody = document.body;
     gradiantbody.style.background = getRandomColor();
 
+    // == Plays audio ==
     let song = document.getElementById("song");
     song.play();
 }

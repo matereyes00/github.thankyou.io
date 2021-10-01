46c6d9c68c762f6648f1e292d3a64c97cfa019e3
Martina Reyes <martina.therese.reyes@obf.ateneo.edu>
2021-10-01 18:42:32 +0800

    Added audio file of song. Modified html, css and js files

--- thankyou/javascript/script.js
+++ thankyou/javascript/script.js
@@ -1,10 +1,6 @@
-/*function getRandomColor() {
-    let colors = [#E0371F, #1FC8E0, #00FF6C, #1FABE0, #D3842C];
-    for (let i = 0; i < colors.length; i++) {
-        let chosenColor = colors[Math.floor(Math.random()*colors.length)];
-        return chosenColor;
-    }
-}*/
+function getRandomColor() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16)
+}
 
 function changeEverything() {
     document.getElementById("GIF-media").src="media/RickAstleyGIF.gif";
@@ -13,7 +9,11 @@ function changeEverything() {
     let msg = document.getElementById("message");
     header.textContent = "You got rickrolled";
     msg.textContent = "Thank you for participating!";
-
+    
     let gradiantbody = document.body;
-    gradiantbody.textContent = getRandomColor();
+    gradiantbody.style.background = getRandomColor();
+
+    let song = document.getElementById("song");
+    song.style.display = 'hidden';
+    song.play();
 }
